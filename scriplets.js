81c6e97d3a76158c690001f6cc61742cf34fa348
DS.undeaD_D <8116188+undeaDD@users.noreply.github.com>
2023-11-22 20:04:38 +0100

    Update scriplets.js

--- scriplets.js
+++ scriplets.js
@@ -1,5 +1,12 @@
 /// Youtube.js
+console.log("[uBO] Scriptlet: Injecting Youtube.js");
 window.addEventListener('load', (event) => {
+  console.log("[uBO] Scriptlet: Page loaded event");
   let linkElem = document.querySelector("a[aria-label='Alle ansehen']");
-  if (linkElem){linkElem.click();}
+  if (linkElem){
+    linkElem.click();
+    console.log("[uBO] Scriptlet: Success");
+  } else {
+    console.log("[uBO] Scriptlet: Error");
+  }
 });
